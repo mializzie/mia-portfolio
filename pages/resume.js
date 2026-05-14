fa19364a024267ceab6bba1c492ad81867ea3e34
mializzie <159646274+mializzie@users.noreply.github.com>
2026-05-13 23:51:50 -0400

    Update resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -30,11 +30,7 @@ const Resume = () => {
         </div>
       )}
       {data.showCursor && <Cursor />}
-      <div
-        className={`container mx-auto mb-10 ${
-          data.showCursor && "cursor-none"
-        }`}
-      >
+      <div className={`container mx-auto mb-10 ${data.showCursor && "cursor-none"}`}>
         <Header isBlog />
         {mount && (
           <div className="mt-10 w-full flex flex-col items-center">
@@ -45,36 +41,28 @@ const Resume = () => {
             >
               <h1 className="text-3xl font-bold">{data.name}</h1>
               <h2 className="text-xl mt-5">{data.resume.tagline}</h2>
-              <h2 className="w-4/5 text-xl mt-5 opacity-50">
-                {data.resume.description}
-              </h2>
+              <h2 className="w-4/5 text-xl mt-5 opacity-50">{data.resume.description}</h2>
               <div className="mt-2">
                 <Socials />
               </div>
               <div className="mt-5">
                 <h1 className="text-2xl font-bold">Experience</h1>
-                {data.resume.experiences.map(
-                  ({ id, dates, type, position, bullets }) => (
-                    <ProjectResume
-                      key={id}
-                      dates={dates}
-                      type={type}
-                      position={position}
-                      bullets={bullets}
-                    />
-                  )
-                )}
+                {data.resume.experiences.map(({ id, dates, type, position, bullets }) => (
+                  <ProjectResume
+                    key={id}
+                    dates={dates}
+                    type={type}
+                    position={position}
+                    bullets={bullets}
+                  />
+                ))}
               </div>
               <div className="mt-5">
                 <h1 className="text-2xl font-bold">Education</h1>
                 <div className="mt-2">
                   <h2 className="text-lg">{data.resume.education.universityName}</h2>
-                  <h3 className="text-sm opacity-75">
-                    {data.resume.education.universityDate}
-                  </h3>
-                  <p className="text-sm mt-2 opacity-50">
-                    {data.resume.education.universityPara}
-                  </p>
+                  <h3 className="text-sm opacity-75">{data.resume.education.universityDate}</h3>
+                  <p className="text-sm mt-2 opacity-50">{data.resume.education.universityPara}</p>
                 </div>
               </div>
               <div className="mt-5">
@@ -85,9 +73,7 @@ const Resume = () => {
                       <h2 className="text-lg">Languages</h2>
                       <ul className="list-disc">
                         {data.resume.languages.map((language, index) => (
-                          <li key={index} className="ml-5 py-2">
-                            {language}
-                          </li>
+                          <li key={index} className="ml-5 py-2">{language}</li>
                         ))}
                       </ul>
                     </div>
@@ -97,9 +83,7 @@ const Resume = () => {
                       <h2 className="text-lg">Frameworks</h2>
                       <ul className="list-disc">
                         {data.resume.frameworks.map((framework, index) => (
-                          <li key={index} className="ml-5 py-2">
-                            {framework}
-                          </li>
+                          <li key={index} className="ml-5 py-2">{framework}</li>
                         ))}
                       </ul>
                     </div>
@@ -109,9 +93,7 @@ const Resume = () => {
                       <h2 className="text-lg">Others</h2>
                       <ul className="list-disc">
                         {data.resume.others.map((other, index) => (
-                          <li key={index} className="ml-5 py-2">
-                            {other}
-                          </li>
+                          <li key={index} className="ml-5 py-2">{other}</li>
                         ))}
                       </ul>
                     </div>
